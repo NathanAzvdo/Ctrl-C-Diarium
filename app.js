7fb4418ffabf60aba63d4032334991d86c404dae
NathanAzvdo <nathan.azevedo28@gmail.com>
2023-12-18 15:05:10 -0300

    adição sessão e config middleware

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 // Configuração
+
+//config sessão
+app.use(session({
+    secret:"secretTest",
+    resave: true,
+    saveUninitialized:true    
+}))
+app.use(flash());
+//body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+//handlebars
 app.engine('handlebars', handlebars.engine({
     defaultLayout: 'main',
     runtimeOptions: {
@@ -18,7 +29,7 @@ app.engine('handlebars', handlebars.engine({
     }
 }));
 app.set('view engine', 'handlebars');
-
+//mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/blogapp').then(()=>{
     console.log('Conected to database');
@@ -35,7 +46,12 @@ app.listen(PORT, () => {
     console.log("running on: localhost:" + PORT);
 });
 
-//arquivos estáticos
+app.use((req, res, next)=>{
+    res.locals.success = req.flash("sucess");
+    res.locals.error = req.flash("error");
+    next();
+})
 
+//arquivos estáticos
 app.use(express.static(path.join(__dirname, "public")));
 //utilizamos o dirname para pegar o caminho absoluto para o diretorio
