d9fbcb528aef857d6b7994d1069e3b695a01c283
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-01-04 19:36:55 -0300

    model de postagens

---
+++ models/Postagens.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Postagens = new Schema({
+    titulo:{
+        type: String,
+        required:true
+    }, 
+    slug:{
+        type:String,
+        required:true
+    },
+    descricao:{
+        type:String,
+        required:true
+    },
+    conteudo:{
+        type:String,
+        required:true
+    },
+    categoria:{
+        type: mongoose.SchemaType.ObjectId,
+        ref:"categorias", 
+        required:true
+    },
+    date:{
+        type:Date, 
+        default:Date.now
+    }
+})
+mongoose.model("Postagens", Postagens);
